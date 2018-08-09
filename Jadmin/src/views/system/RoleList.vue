2b7b8d93db95ac729f5f90be3c235c30bf059dbd
jacksonhou <houyongbo03@163.com>
2018-08-09 23:17:30 +0800

    测试代码

--- Jadmin/src/views/system/RoleList.vue
+++ Jadmin/src/views/system/RoleList.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="app-container">
+    <el-card class="box-card">
+      <el-form :inline="true" :model="formInline">
+        <el-form-item label="审批人">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="formInline.region" placeholder="活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
     <el-form ref="form" :model="form" label-width="80px">
       <el-form-item label="活动名称">
         <el-input v-model="form.name"></el-input>
@@ -51,23 +67,27 @@
 export default {
   name: "RoleList",
   data() {
-      return {
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formInline: {
+        user: '',
+        region: ''
       }
     }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    }
+  }
 };
 </script>
