3831e931667667bbfe6070801a8e6493c855612c
jacksonhou <houyongbo03@163.com>
2018-08-14 09:04:22 +0800

    简化代码

--- Jadmin/src/utils/token.js
+++ Jadmin/src/utils/token.js
@@ -1,13 +1,17 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
+const TokenKey = 'User-Token'
+// 有效期 1 天
+const expires = 1
 
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, token, {
+    expires: expires
+  })
 }
 
 export function removeToken() {
