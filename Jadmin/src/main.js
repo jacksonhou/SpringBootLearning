056056fd4b1655e59d41ba6839cbde54f929186d
jacksonhou <houyongbo03@163.com>
2018-08-17 09:42:37 +0800

    简化代码

--- Jadmin/src/main.js
+++ Jadmin/src/main.js
@@ -17,17 +17,18 @@ import './icons' // icon
 import './errorLog' // error log
 import './permission' // permission control
 // import './mock' // simulation data
-import { hasPermission } from './utils/hasPermission'
+import {
+  hasPermission
+} from './utils/hasPermission'
 
 import * as filters from './filters' // global filters
 import $ from 'jquery'
-import './assets/bootstrap-3.3.7/css/bootstrap.min.css'
-import './assets/bootstrap-3.3.7/js/bootstrap.min.js'
+import '@/assets/css/common.css'
 
 Vue.use(Element, {
-    size: 'medium', // set element-ui default size
-    // i18n: (key, value) => i18n.t(key, value),
-    locale
+  size: 'medium', // set element-ui default size
+  // i18n: (key, value) => i18n.t(key, value),
+  locale
 })
 
 // 全局的常量
@@ -35,15 +36,15 @@ Vue.prototype.hasPermission = hasPermission
 
 // register global utility filters.
 Object.keys(filters).forEach(key => {
-    Vue.filter(key, filters[key])
+  Vue.filter(key, filters[key])
 })
 
 Vue.config.productionTip = false
 
 new Vue({
-    el: '#app',
-    router,
-    store,
-    i18n,
-    render: h => h(App)
+  el: '#app',
+  router,
+  store,
+  i18n,
+  render: h => h(App)
 })
